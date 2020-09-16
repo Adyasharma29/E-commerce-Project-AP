24eb36a7bce6aeddc016c2287f566cfeb1b97700
zahir1509 <70563556+zahir1509@users.noreply.github.com>
2020-09-16 21:01:28 +0530

    Created Data.js and Rendered Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import data from './data';
 import './App.css';
 
 function App() {
@@ -20,9 +21,9 @@ function App() {
                     <a href="index.html">tronix</a>
                 </div>
                 <div className="header_links">
-                    <a href="wishlist.html">Wishlist</a>
-                    <a href="mycart.html">My Cart</a>
-                    <a href="register.html">Register</a>
+                    <a href="wishlist.html">Wishlist </a>
+                    <a href="mycart.html">My Cart </a>
+                    <a href="register.html">Register </a>
                 </div>
             </header>
             <aside className="sidebar">
@@ -41,42 +42,24 @@ function App() {
                 </ul>
             </aside>
             <main className="main">
-                <div className="content">
-                   <ul className="products">
+                <div className="products">
+                   {
+                       data.products.map(product =>
+                     
                     <li> 
                         <div className="product1">
-                            <img className="product_image" src="/images/d1.jpg" alt="product1" />
+                            <img className="product_image" src={product.image} alt="product1" />
                             <div className="product_name">
-                                <a href="product.html">MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray</a>
+                                <a href="product.html">{product.name}</a>
                             </div>
-                            <div className="product_brand">Apple</div>
-                            <div className="product_price">₹70,000</div>
-                            <div className="product_rating">5 Stars (10 Reviews)</div>
+                            <div className="product_brand">{product.brand}</div>
+                            <div className="product_price">₹{product.price}</div>
+                            <div className="product_rating">{product.rating} Stars ({product.numReviews} Reviews) </div>
                         </div>
-                    </li>
-                    <li> 
-                        <div className="product1">
-                            <img className="product_image" src="/images/d1.jpg" alt="product1" />
-                            <div className="product_name">
-                                <a href="product.html">MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray</a>
-                            </div>
-                            <div className="product_brand">Apple</div>
-                            <div className="product_price">₹70,000</div>
-                            <div className="product_rating">4.5 Stars (10 Reviews)</div>
-                        </div>
-                    </li>
-                    <li> 
-                        <div className="product1">
-                            <img className="product_image" src="/images/d1.jpg" alt="product1" />
-                            <div className="product_name">
-                                <a href="product.html">MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray</a>
-                            </div>
-                            <div className="product_brand">Apple</div>
-                            <div className="product_price">₹70,000</div>
-                            <div className="product_rating">4.5 Stars (10 Reviews)</div>
-                        </div>
-                    </li>
-                </ul> 
+                    </li>)
+}
+
+                
                 </div>
                 
             </main>
---
+++ frontend/src/data.js
@@ -0,0 +1,59 @@
+export default {
+    products:[
+        {
+        name: 'MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray',
+        category: 'Laptop',
+        image: '/images/d1.jpg',
+        price: 70000,
+        brand: 'Apple',
+        rating: 5,
+        numReviews: 10
+    },
+    {
+        name: 'MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray',
+        category: 'Laptop',
+        image: '/images/d1.jpg',
+        price: 70000,
+        brand: 'Apple',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray',
+        category: 'Laptop',
+        image: '/images/d1.jpg',
+        price: 70000,
+        brand: 'Apple',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray',
+        category: 'Laptop',
+        image: '/images/d1.jpg',
+        price: 70000,
+        brand: 'Apple',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray',
+        category: 'Laptop',
+        image: '/images/d1.jpg',
+        price: 70000,
+        brand: 'Apple',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray',
+        category: 'Laptop',
+        image: '/images/d1.jpg',
+        price: 70000,
+        brand: 'Apple',
+        rating: 4.5,
+        numReviews: 10
+    },
+    
+]
+}
