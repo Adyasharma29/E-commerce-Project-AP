a56775195250a86b65dfd41ee913118170c4313b
zahir1509 <70563556+zahir1509@users.noreply.github.com>
2020-09-16 21:06:18 +0530

    Update data.js

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -36,24 +36,7 @@ export default {
         rating: 4.5,
         numReviews: 10
     },
-    {
-        name: 'MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray',
-        category: 'Laptop',
-        image: '/images/d1.jpg',
-        price: 70000,
-        brand: 'Apple',
-        rating: 4.5,
-        numReviews: 10
-    },
-    {
-        name: 'MacBook Air (8GB RAM, 256GB SSD Storage) - Space Gray',
-        category: 'Laptop',
-        image: '/images/d1.jpg',
-        price: 70000,
-        brand: 'Apple',
-        rating: 4.5,
-        numReviews: 10
-    },
-    
+
+
 ]
 }
